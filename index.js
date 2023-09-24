199c0ee9a5973b47024a01d8d33d74edcf9ee655
anitta-sleiman <sleiman3198@gmail.com>
2023-09-24 08:42:26 +0300

    Step 3

--- index.js
+++ index.js
@@ -5,4 +5,27 @@ app.get("/", function (req, res) {
   res.send("ok");
 });
 
-app.listen(3000);
+// /test url route
+app.get("/test", function (req, res) {
+  const response = {
+    status: 200,
+    message: "ok",
+  };
+  res.json(response);
+});
+
+// /time url route
+app.get("/time", function (req, res) {
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+  const response = {
+    status: 200,
+    message: currentTime,
+  };
+  res.json(response);
+});
+
+app.listen(3200);
